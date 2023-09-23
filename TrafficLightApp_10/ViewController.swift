09e8cae897d5fdc5c20d243b68c5d659b652ecfb
Andrey <akovalev@me.com>
2023-09-24 01:04:48 +0300

    This is not completed exercise. However, I will continue work on it

--- TrafficLightApp_10/ViewController.swift
+++ TrafficLightApp_10/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    private var currentLightIndex = 0
+    private var lights: [UIView] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.layer.cornerRadius = 60
+        yellowLight.layer.cornerRadius = 60
+        greenLight.layer.cornerRadius = 60
+        startButton.layer.cornerRadius = 15
+        
+        redLight.alpha = 0.0
+        yellowLight.alpha = 0.0
+        greenLight.alpha = 0.0
+
+        lights = [redLight, yellowLight, greenLight]
     }
 
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        lights[currentLightIndex].alpha = 1.0
+        if currentLightIndex > 0 {
+            lights[currentLightIndex - 1].alpha = 0.3
+        } else if currentLightIndex == 0 && sender.currentTitle == "NEXT" {
+           
+            lights[lights.count - 1].alpha = 0.3
+        }
 
+        sender.setTitle("NEXT", for: .normal)
+
+        currentLightIndex = (currentLightIndex + 1) % lights.count
+    }
 }
-
