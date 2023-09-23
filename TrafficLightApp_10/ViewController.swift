e77b3aaef1b277faa6d54325242b283fd3c5f5c6
Andrey <akovalev@me.com>
2023-09-24 00:44:56 +0300

    Initial Commit

---
+++ TrafficLightApp_10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLightApp_10
+//
+//  Created by Andrey Kovalev on 24.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
